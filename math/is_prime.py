851f48418d8354176a5edee1f1b66ff9943f9366
gaku <78236427+neko0774@users.noreply.github.com>
2022-02-20 12:19:25 +0100

    updated to be faster

--- math/is_prime.py
+++ math/is_prime.py
@@ -1,7 +1,10 @@
 def is_prime(number):
-    i = 2
+    if i < 2: return 0
+    if i==2: return 1
+    if i%2==0: return 0
+    i = 3
     while i*i <= number:
         if number%i==0:
             return 0
-        i+=1
+        i+=2
     return 1
